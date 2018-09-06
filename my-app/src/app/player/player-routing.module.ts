2012f08b000331b22e8f58552af7bcc0d7d7eb2c
Khanh Nguyen <nvkhanh08t4@gmail.com>
2018-09-06 22:50:50 +0700

    Fix bug routing.

--- my-app/src/app/player/player-routing.module.ts
+++ my-app/src/app/player/player-routing.module.ts
@@ -4,16 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PlayerModule } from './../player/player.module';
 import { PlayerDetailComponent } from './../player/pages/player-detail/player-detail.component'
+import { PlayerComponent } from './pages/player/player.component';
 
 export const routesConfigPlayer: Routes = [
   {
+    //path: 'team/:id/player',
     path: 'player',
-    component: PlayerDetailComponent,
-    // children: [
-    //   { path: 'detail', component: PlayerDetailComponent },
-    //   { path: 'edit', component:  PlayerDetailComponent },
-    //   { path: 'new', component:  PlayerDetailComponent }
-    // ]
+   // component: PlayerDetailComponent,
+    children: [
+      { path: '', redirectTo: 'edit', pathMatch: 'full' },
+      { path: 'detail', component: PlayerComponent },
+      { path: 'edit', component: PlayerDetailComponent },
+      { path: 'new', component:  PlayerComponent }
+    ]
   }
 ];
 
